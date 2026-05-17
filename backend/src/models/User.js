140b675fe11336488dc424856bea39e6c4c397d7
TranAnhHuy <anhhuyrubic@gmail.com>
2026-05-18 00:44:39 +0700

    chore: install frontend dependencies in node_modules

---
+++ backend/src/models/User.js
@@ -0,0 +1,38 @@
+const { DataTypes } = require('sequelize');
+const { sequelize } = require('../config/database');
+
+const User = sequelize.define('User', {
+    id: {
+        type: DataTypes.INTEGER.UNSIGNED,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    name: {
+        type: DataTypes.STRING(100),
+        allowNull: false
+    },
+    email: {
+        type: DataTypes.STRING(150),
+        allowNull: false,
+        unique: true,
+        validate: { isEmail: true }
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    role: {
+        type: DataTypes.ENUM('customer', 'admin'),
+        allowNull: false,
+        defaultValue: 'customer'
+    },
+    avatar: {
+        type: DataTypes.STRING,
+        allowNull: true
+    }
+}, {
+    tableName: 'users',
+    timestamps: true
+});
+
+module.exports = User;
